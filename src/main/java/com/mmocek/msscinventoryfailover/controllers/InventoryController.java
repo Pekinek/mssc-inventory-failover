b32b6dc961e2143ca5e45df5aeb1c07536781cfc
Michał Mocek <pekinhere@gmail.com>
2021-12-07 22:55:45 +0100

    resilience4j added

--- src/main/java/com/mmocek/msscinventoryfailover/controllers/InventoryController.java
+++ src/main/java/com/mmocek/msscinventoryfailover/controllers/InventoryController.java
@@ -3,7 +3,6 @@ package com.mmocek.msscinventoryfailover.controllers;
 import com.mmocek.commons.model.BeerInventoryDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -13,23 +12,13 @@ import java.util.UUID;
 @RestController
 public class InventoryController {
 
-    @GetMapping("api/v1/beer/{beerId}/inventory")
-    public Flux<BeerInventoryDto> listBeersById(@PathVariable UUID beerId) {
-        log.debug("Finding Inventory for beerId: " + beerId);
-        return getFailoverInventory();
-    }
-
-    @GetMapping("api/v1/beerUpc/{beerUpc}/inventory")
-    public Flux<BeerInventoryDto> listBeersByUpc(@PathVariable String beerUpc) {
-        log.debug("Finding Inventory for beerUpc: " + beerUpc);
-        return getFailoverInventory();
-    }
-
-    private Flux<BeerInventoryDto> getFailoverInventory() {
+    @GetMapping("inventory-failover")
+    public Flux<BeerInventoryDto> listBeersById() {
         BeerInventoryDto beerInventoryDto = BeerInventoryDto.builder()
                                                             .quantityOnHand(999)
                                                             .beerId(new UUID(0, 0))
                                                             .build();
         return Flux.just(beerInventoryDto);
     }
+
 }
